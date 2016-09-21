007c4f06ff0926ddaaca1ee8dcdc4bb02eff215b
Gagandeep Garg <gagandeepgarg6@gmail.com>
2016-09-21 12:04:56 -0300

    Add files via upload

---
+++ Factory-Design-pattern/ClanPizza.java
@@ -0,0 +1,12 @@
+package SimpleFactory;
+
+public class ClanPizza extends Pizza {
+
+	public ClanPizza() {
+		name = "Clam Pizza";
+		dough = "Thin crust";
+		sauce = "White garlic sauce";
+		toppings.add("Clams");
+		toppings.add("Grated parmesan cheese");
+	}
+}
---
+++ Factory-Design-pattern/PizzaTestDrive.java
@@ -0,0 +1,21 @@
+package SimpleFactory;
+
+public class PizzaTestDrive {
+
+	public static void main(String[] args) {
+		
+		SimplePizzaFactory factory = new SimplePizzaFactory();
+		PizzaStore store = new PizzaStore(factory);
+
+		Pizza pizza = store.orderPizza("cheese");
+		System.out.println("We ordered a " + pizza.getName() + "\n");
+		System.out.println(pizza);
+		
+		
+		pizza = store.orderPizza("clan");
+		System.out.println("We ordered a " + pizza.getName() + "\n");
+		System.out.println(pizza);
+
+	}
+
+}
