d03659b942d966dffee81130e8444786b1204c97
Gagandeep Garg <gagandeepgarg6@gmail.com>
2016-09-22 11:53:57 -0300

    Add files via upload
    factory method designing

---
+++ Method-factory-pattern/ChicagoPizzaStore.java
@@ -0,0 +1,20 @@
+package FactoryMethod;
+
+public class ChicagoPizzaStore extends PizzaStore{
+
+	@Override
+	protected Pizza createPizza(String item) {
+		   if (item.equals("cheese")) {
+            		return new ChicagoStyleCheesePizza();
+        	} else if (item.equals("veggie")) {
+        	    	return new ChicagoStyleVeggiePizza();
+        	} else if (item.equals("clam")) {
+        	    	return new ChicagoStyleClamPizza();
+        	} else if (item.equals("pepperoni")) {
+            		return new ChicagoStylePepperoniPizza();
+        	} else return null;
+	
+	}
+
+ }
+	
---
+++ Method-factory-pattern/Pizza.java
@@ -0,0 +1,49 @@
+package FactoryMethod;
+import java.util.ArrayList;
+
+abstract public class Pizza {
+	String name;
+	String dough;
+	String sauce;
+	ArrayList<String> toppings = new ArrayList<String>();
+
+	public String getName() {
+		return name;
+	}
+
+	void prepare() {
+		System.out.println("Prepare " + name);
+		System.out.println("Tossing dough...");
+		System.out.println("Adding sauce...");
+		System.out.println("Adding toppings: ");
+		for (String topping : toppings) {
+			System.out.println("   " + topping);
+		}
+	}
+	public void bake() {
+		System.out.println("Baking " + name);
+	}
+
+	public void cut() {
+		System.out.println("Cutting " + name);
+	}
+
+	public void box() {
+		System.out.println("Boxing " + name);
+	}
+	
+	public String toString() {
+		// code to display pizza name and ingredients
+		StringBuffer display = new StringBuffer();
+		display.append("---- " + name + " ----\n");
+		display.append(dough + "\n");
+		display.append(sauce + "\n");
+		for (String topping : toppings) {
+			display.append(topping + "\n");
+		}
+		return display.toString();
+	
+	
+
+}
+}
